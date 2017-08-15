98b2efac60700986715ae41c85e7f1b2bfe7b50f
Brendan Walker <brendan.walker@pelotoncycle.com>
2017-08-15 15:02:32 -0400

    Adding has_bit and top_tables_by_memory functions

---
+++ functions.sql
@@ -0,0 +1,24 @@
+-- Allows for finding the tables with the highest memory usage
+-- Can be called by `select * from top_tables_by_memory(10)`
+
+CREATE TYPE table_info AS (relation text, total_size text);
+CREATE FUNCTION top_tables_by_memory (limit_value integer) 
+RETURNS table_info AS $$
+  SELECT nspname || '.' || relname AS "relation",
+      pg_size_pretty(pg_total_relation_size(C.oid)) AS "total_size"
+    FROM pg_class C
+    LEFT JOIN pg_namespace N ON (N.oid = C.relnamespace)
+    WHERE nspname NOT IN ('pg_catalog', 'information_schema')
+      AND C.relkind <> 'i'
+      AND nspname !~ '^pg_toast'
+    ORDER BY pg_total_relation_size(C.oid) DESC
+    LIMIT limit_value;
+$$ LANGUAGE sql;
+
+--Allows for checking if a bitstring has a specific bit set. Bit position is counted from the left
+-- Can be called by `bit_field &>> 3` or `has_bit(bit_field, 3)`
+CREATE FUNCTION has_bit (a bit varying, b integer) 
+RETURNS boolean AS $$ 
+  SELECT (b < bit_length(a)) AND ((a & (repeat('0', b) || '1' || repeat('0', bit_length(a) - (b + 1)))::bit varying) != repeat('0', bit_length(a))::bit varying);
+$$ LANGUAGE sql;
+CREATE OPERATOR &>> (leftarg = bit varying, rightarg = integer, procedure = has_bit);
